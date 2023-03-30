2aa1a2255c78d5a9e45dd3d4c022a06ec881d598
Mamidu Moshood <mamidumoshood1990@gmail.com>
2023-03-30 04:50:46 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializes the size"""
+
+        self.__size = size
+
